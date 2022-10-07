9b00cd338b354d3a983de3aa7f02612e97c1e74b
vaibhav <vaibhavkumarjha46@gmail.com>
2022-10-07 17:52:09 +0530

    Handling Errors

--- app.js
+++ app.js
@@ -55,7 +55,16 @@ app.use((req, res, next) => {
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphResolver,
-    graphiql: true
+    graphiql: true,
+    formatError(err) {
+        if (!err.originalError) {
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'An Error Occured.';
+        const code = err.originalError.code || 500;
+        return { message: message, status: code, data: data };
+    }
 }))
 
 app.use((error, req, res, next) => {
