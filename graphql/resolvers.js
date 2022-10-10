2fbcbec5eb2a9c0cac80db294a174a024db02cba
vaibhav <vaibhavkumarjha46@gmail.com>
2022-10-10 16:42:19 +0530

    Adding a Get Post Query & Resolver

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -93,5 +93,25 @@ module.exports = {
         user.posts.push(createdPost);
         await user.save();
         return { ...createdPost._doc, _id: createdPost._id.toString(), createdAt: createdPost.createdAt.toISOString(), updatedAt: createdPost.updatedAt.toISOString() }
+    },
+    posts: async function (args, req) {
+        if (!req.isAuth) {
+            const error = new Error('Not Authenticated');
+            error.code = 401;
+            throw error;
+        }
+        const totalPosts = await Post.find().countDocuments();
+        const posts = await Post.find().sort({ createdAt: -1 }).populate('creator');
+
+        return {
+            posts: posts.map(p => {
+                return {
+                    ...p._doc,
+                    _id: p._id.toString(),
+                    createdAt: p.createdAt.toISOString(),
+                    updatedAt: p.updatedAt.toISOString()
+                }
+            }), totalPosts: totalPosts
+        }
     }
 };
