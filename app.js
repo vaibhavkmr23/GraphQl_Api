7359edd3a4cc737e8073a40e300383482e5a667f
vaibhav <vaibhavkumarjha46@gmail.com>
2022-10-10 12:42:45 +0530

    Extracting User Data From the Auth Token

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const { graphqlHTTP } = require('express-graphql')
 
 const graphqlSchema = require('./graphql/schema');
 const graphResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
 
 // const { v4: uuidv4 } = require('uuid');
 
@@ -55,6 +56,8 @@ app.use((req, res, next) => {
     }
     next();
 });
+
+app.use(auth);
 
 app.use('/graphql', graphqlHTTP({
     schema: graphqlSchema,
